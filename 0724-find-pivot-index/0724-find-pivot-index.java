19e331509ef10a0c26133bf0a01e65325880d660
Amrita Sinha <amritasinha0028@gmail.com>
2026-05-04 21:12:25 +0530

    Time: 315 ms (6.03%), Space: 47.1 MB (92.57%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        for(int i = 0; i < nums.length; i++){
+
+            int left = 0;
+        
+            for(int j = 0; j < i; j++){
+                left += nums[j];
+            }
+
+            int right = 0;
+            
+            for(int j = i + 1; j < nums.length; j++){
+                right += nums[j];
+            }
+
+            if(left == right){
+                return i;
+            }
+        }
+        return -1;
+    }
+}

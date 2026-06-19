1788a79b4777e513da8919d5ccaf10c204da8854
Amrita Sinha <amritasinha0028@gmail.com>
2026-06-19 10:11:29 +0530

    Time: 0 ms (100%), Space: 43.3 MB (18.48%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,13 +1,16 @@
 class Solution {
     public int largestAltitude(int[] gain) {
         int start=0;
-        int max=0;
+        int maxx=0;
         for(int i=0;i<gain.length;i++){
             start=start+gain[i];
-            if(start>max){
-                max=start;
-            }
+           // if(start>max){
+             //   max=start;}
+             maxx=Math.max(maxx,start);
+
+            
         }
-        return max;
+        return maxx;
+        
     }
 }

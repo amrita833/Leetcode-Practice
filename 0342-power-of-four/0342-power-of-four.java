b86404105ffc7cf7c97a6603d6a1c758080981a6
Amrita Sinha <amritasinha0028@gmail.com>
2026-06-22 23:37:52 +0530

    Time: 0 ms (100%), Space: 42.4 MB (84.5%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        
+        
+        while(n>0){
+            if(n%4==0){
+            n=n/4;
+        }
+        else{
+            break;
+        }}
+        if(n==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}

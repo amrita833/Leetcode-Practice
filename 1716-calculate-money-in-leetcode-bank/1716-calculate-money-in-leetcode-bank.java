7eef1d3c50410f64e2a5e7dc7e1b1802b7a87993
Amrita Sinha <amritasinha0028@gmail.com>
2026-06-23 08:44:51 +0530

    Time: 2 ms (26.12%), Space: 42.1 MB (64.71%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int totalMoney(int n) {
+        int total=0;
+        int day=0;
+        int monday=1;
+        for(int i=1;i<=n;i++){
+            total=total+monday+day;
+            day++;
+            if(day==7){
+                day=0;
+                monday++;
+            }
+        }
+        return total;
+
+    }
+}

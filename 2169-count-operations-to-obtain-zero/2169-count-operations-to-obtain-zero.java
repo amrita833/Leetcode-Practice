4cbb86722878812bb65d62822f59127582827253
Amrita Sinha <amritasinha0028@gmail.com>
2026-06-24 08:26:45 +0530

    Time: 1 ms (84.69%), Space: 42.4 MB (30.62%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int count=0;
+        Boolean found=false;
+        while(num1>0 && num2>0){
+            if(num1>=num2){
+             num1=  num1-num2;
+                count++;
+            }
+            else if(num1<num2){
+              num2=  num2-num1;
+                count++;
+            }
+            if(count==0){
+                found=true;
+            }
+        }
+        return count;
+    }
+}

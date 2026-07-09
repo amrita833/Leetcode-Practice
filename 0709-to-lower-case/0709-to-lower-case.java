15bdeabb46f054ab55c644ecb209908b6f779ff5
Amrita Sinha <amritasinha0028@gmail.com>
2026-07-09 09:31:28 +0530

    Time: 1 ms (26.12%), Space: 42.9 MB (51.45%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String toLowerCase(String s) {
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+
+        }
+        return s.toLowerCase();
+    }
+}
